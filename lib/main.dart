09d0601bb67e61e57d844a849cd5d7e302dd37e4
Dresdain <dresdain@live.com>
2023-05-05 15:58:20 +0800

    Add Login and Sign up buttons
    - Added Raleway light
    - added color_extension.dart for future use

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:txiapp/extensions/color_extension.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/BGImage.png'),
             fit: BoxFit.cover,
@@ -39,36 +40,103 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SizedBox(
-                width: 200,
-                height: 200,
+                width: 300,
+                height: 300,
                 child: SvgPicture.asset(
                   'assets/images/LogoTXI.svg',
-                  color: Color(0xFFD6AD67),
+                  color: const Color(0xFFD6AD67),
                 ),
               ),
-              Text(
+              const Text(
                 'WELCOME',
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: 34,
                   fontFamily: 'Raleway',
-                  fontWeight: FontWeight.normal,
+                  fontWeight: FontWeight.w100,
                   color: Colors.white,
                 ),
               ),
-              SizedBox(
-                width: 300,
-                height: 20,
+              Container(
+                margin: const EdgeInsets.only(bottom: 50.0),
                 child: SvgPicture.asset(
                   'assets/images/LineDot.svg',
-                  color: Color(0xFFD6AD67),
+                  width: 300,
+                  height: 25,
+                  color: const Color(0xFFD6AD67),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your log in logic here
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(const Color(0xFFD6AD67)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                ),
+                child: const SizedBox(
+                  width: 250,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      'LOG IN',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              OutlinedButton(
+                onPressed: () {
+                  // Add your sign up logic here
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.transparent), // transparent background
+                  side: MaterialStateProperty.all<BorderSide>(
+                    const BorderSide(color: Color(0xFFD6AD67)), // outline color
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                ),
+                child: const SizedBox(
+                  width: 250,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      'SIGN UP',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFD6AD67), // text color
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
         ),
-        
       ),
     );
-
   }
 }
