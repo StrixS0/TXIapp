ddd1877d9335ffb315f05615e1ff6757591123fa
Dresdain <dresdain@live.com>
2023-05-18 06:18:38 +0800

    Add select vehicle

---
+++ lib/presentation/select_vehicle/select_vehicle.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+//Components
+class SelectVehicle extends StatelessWidget {
+  const SelectVehicle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity, // Fill the screen height
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/BGImage.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 320),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: SvgPicture.asset(
+                          'assets/images/LogoTXI.svg',
+                          color: const Color(0xFFD6AD67),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          // Handle menu bar icon tap
+                        },
+                        icon: Icon(Icons.menu),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'SELECT A VEHICLE',
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w100,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 300),
+                  child: GridView.count(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    children: [
+                      _buildVehicleBox('SEDAN'),
+                      _buildVehicleBox('SUV'),
+                      _buildVehicleBox('LUXURY SUV'),
+                      _buildVehicleBox('SPRINTER'),
+                      _buildVehicleBox('BUS'),
+                      _buildVehicleBox('EXECUTIVE BUS'),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    'Back',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w100,
+                      color: Color(0xFFD6AD67),
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 80),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildVehicleBox(String vehicleName) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          // Handle onTap for the vehicle box
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.white,
+              width: 0.5,
+            ),
+          ),
+          child: Container(
+            color: Color.fromRGBO(255, 255, 255, 0.3),
+            height: 50,
+            width: 50,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  vehicleName,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
