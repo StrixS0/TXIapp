b132056c84751d85b2057b88a25f41b2449e3582
Dresdain <dresdain@live.com>
2023-05-26 21:37:27 +0800

    Add passenger screens

---
+++ lib/presentation/components/dropdown.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+Widget buildDropdown({
+  List<String>? values,
+  String? defaultValue,
+  ValueChanged<String?>? onChanged,
+}) {
+  values ??= ['-']; // Use default value if values is null
+
+  return Container(
+    constraints: const BoxConstraints(maxWidth: 300, maxHeight: 60),
+    child: DropdownButtonFormField<String>(
+      value: defaultValue,
+      items: values.map((String dropdownValue) {
+        return DropdownMenuItem<String>(
+          value: dropdownValue,
+          child: Center(
+            child: Text(
+              dropdownValue,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+          ),
+        );
+      }).toList(),
+      onChanged: onChanged,
+      dropdownColor: const Color.fromARGB(255, 137, 109, 61),
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 10),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xFFD6AD67),
+            width: 1.0,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xFFD6AD67),
+            width: 1.0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Color(0xFFD6AD67),
+            width: 1.0,
+          ),
+        ),
+        filled: true,
+        fillColor: Color.fromRGBO(42, 42, 42, 0.473),
+      ),
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontFamily: 'Raleway',
+        fontWeight: FontWeight.w100,
+      ),
+      isDense: false,
+      isExpanded: true,
+      itemHeight: 55,
+      hint: const Center(
+        child: Text(
+          '-',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w100,
+          ),
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/presentation/passenger_exec_sprinter/passenger_exec_sprinter.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:txiapp/presentation/components/buttons.dart';
+import 'package:txiapp/presentation/components/dropdown.dart'; 
+
+// Components
+class PassengerExecSprinter extends StatefulWidget {
+  const PassengerExecSprinter({Key? key}) : super(key: key);
+
+  @override
+  _PassengerExecSprinterState createState() => _PassengerExecSprinterState();
+}
+
+class _PassengerExecSprinterState extends State<PassengerExecSprinter> {
+  int selectedOption = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity, // Fill the screen height
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/BGImage.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 320),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: SvgPicture.asset(
+                          'assets/images/LogoTXI.svg',
+                          color: const Color(0xFFD6AD67),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          // Handle menu bar icon tap
+                        },
+                        icon: const Icon(Icons.menu),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 300),
+                  child: const Center(
+                    child: Text(
+                      'EXECUTIVE SPRINTER',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Center(
+                  child: Text(
+                    'HOW MANY PASSENGERS?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w100,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 300),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const SizedBox(width: 25),
+                      Expanded(
+                        child: buildDropdown(
+                              onChanged: (value) {
+                                // onEvent(StateChanged(value!));
+                              },
+                              values: ["1 - 4 pax", "5 pax", "6 pax","7 pax","8 pax","9 pax","10 pax","11 pax","12 pax","13 pax","14 pax"],
+                            ),
+                      ), 
+                      const SizedBox(width: 25),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 0.0),
+                  child: SvgPicture.asset(
+                    'assets/images/LineDot.svg',
+                    width: 300,
+                    height: 25,
+                    color: const Color(0xFFD6AD67),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 150),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          CustomRadio(
+                            value: 0,
+                            groupValue: selectedOption,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedOption = value!;
+                              });
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          const Expanded(
+                            child: Text(
+                              'No luggage',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'Raleway',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          CustomRadio(
+                            value: 1,
+                            groupValue: selectedOption,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedOption = value!;
+                              });
+                            },
+                          ),
+                          const SizedBox(width: 10),
+                          const Expanded(
+                            child: Text(
+                              'With luggage',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontFamily: 'Raleway',
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                PrimaryElevatedButton(
+                  onPressed: () {
+                    // onEvent(FormSubmitted());
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //       builder: (context) => const AddPaymentMethod()),
+                    // );
+                  },
+                  text: 'Next',
+                  // text: signupPersonalState.loading ? 'Please wait...' : 'Continue',
+                ),
+                const SizedBox(height: 30),
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    'Back',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w100,
+                      color: Color(0xFFD6AD67),
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 80),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomRadio<T> extends StatelessWidget {
+  final T value;
+  final T? groupValue;
+  final ValueChanged<T?>? onChanged;
+
+  const CustomRadio({
+    required this.value,
+    required this.groupValue,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onChanged?.call(value);
+      },
+      child: Container(
+        width: 24,
+        height: 24,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          border: Border.all(
+            color: groupValue == value ? const Color(0xFFD6AD67) : const Color(0xFFD6AD67),
+            width: 2,
+          ),
+        ),
+        child: Center(
+          child: groupValue == value
+              ? Container(
+                  width: 12,
+                  height: 12,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFD6AD67),
+                  ),
+                )
+              : null,
+        ),
+      ),
+    );
+  }
+}
