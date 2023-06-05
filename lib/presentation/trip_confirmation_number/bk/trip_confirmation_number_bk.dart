30e1dc73f6021a74976b3c4f64aadfb641f8cda1
Dresdain <dresdain@live.com>
2023-06-05 08:47:13 +0800

    Add Trip Confirmation Number screen

---
+++ lib/presentation/trip_confirmation_number/bk/trip_confirmation_number_bk.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart'; 
+import 'package:txiapp/presentation/components/buttons.dart'; 
+
+//Components
+class BKTripConfirmationNumber extends StatelessWidget {
+  const BKTripConfirmationNumber({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) { 
+    return Container(
+      height: double.infinity, // Fill the screen height
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/BGImage.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 320),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: SvgPicture.asset(
+                          'assets/images/LogoTXI.svg',
+                          color: const Color(0xFFD6AD67),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          // Handle menu bar icon tap
+                        },
+                        icon: Icon(Icons.menu),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Center(
+                  child: Text(
+                    'THANK YOU',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w100,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SvgPicture.asset(
+                  'assets/images/LineDot.svg',
+                  width: 300,
+                  height: 25,
+                  color: const Color(0xFFD6AD67),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'THIS IS YOUR TRIP CONFIRMATION NUMBER',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w100,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const SizedBox(height: 10),
+                OutlinedButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(17, 0, 0, 0)),
+                    side: MaterialStateProperty.all<BorderSide>(
+                      const BorderSide(color: Color(0xFFD6AD67)),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: 270,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 25,
+                        horizontal: 25,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Text(
+                                'Confirmation Number:',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontFamily: 'NoirPro',
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xFFD6AD67),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '334110',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 26,
+                                  fontFamily: 'NoirPro',
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          )),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                PrimaryElevatedButton(
+                  onPressed: () {},
+                  text: 'Book another trip?',
+                  // text: signupPersonalState.loading ? 'Please wait...' : 'Continue',
+                ),
+                const SizedBox(height: 80),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildVehicleBox(String vehicleName) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          // Handle onTap for the vehicle box
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.white,
+              width: 1,
+            ),
+          ),
+          child: Container(
+            color: Color.fromRGBO(255, 255, 255, 0.3),
+            height: 50,
+            width: 50,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  vehicleName,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LilSpacer extends StatelessWidget {
+  const LilSpacer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 10);
+  }
+}
